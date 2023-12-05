c89526f479e748f83dd984d3aa91821810aa2499
Felix <softwareaastu23@gmail.com>
2023-12-05 09:34:08 +0300

    =first commit

---
+++ note2.py
@@ -0,0 +1,47 @@
+# Indentition is very important in python
+# indentition is all about to get rid of enclosing brackets {} wu use in other programming langaue.
+# EXAMPLE-1
+"""if 1>2: #colon indicate code block here
+    print("YES")
+    if 2<3: # NESTED IF STATMENTS
+        print("TRUE")
+
+# EXAMPLE-2"""
+"""
+if 40 < 40:
+    print("#ROOM-05")
+elif 30 < 30:
+    print("#ROOM-06")
+else:
+    print("No room is reserved!!")
+
+
+# IF ELSE STATMENTS
+if 100 > 200:
+    print("Welcome")
+else:
+    print("I owe you nothing!!")
+
+# adding conditions using 'elif'
+if "hello" =="HELLO":
+    print("No")
+elif "hello"=="hello":
+    print("Yes")
+else:
+    print("dumb and dumber!!")
+    """
+age = int(input("Please enter the customer age:"))
+if age <18:
+    print("your not eligible for driving, because you are: "+str(age))
+elif age >=18:
+    print("Your allowed take dri ving exam!!")
+else:
+    print("Go home!!")
+
+
+
+
+
+
+
+
